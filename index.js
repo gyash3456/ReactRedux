f142e7d1d484020d0f46adab371c260f8fe2e773
gyash3456 <gyash3456@gmail.com>
2022-05-26 14:56:26 +0530

    cake ordered basic react

---
+++ index.js
@@ -0,0 +1,35 @@
+const redux = require('redux'); 
+const createStore = redux.createStore;
+const CAKE_ORDERED = 'CAKE_ORDERED'
+
+function orderCake(){
+    return{
+        type:CAKE_ORDERED,
+        quantity:1,
+    }
+}
+const initialState ={
+    numOfCakes:10,
+}
+const reducer = (state=initialState,action)=>{
+    switch(action.type){
+        case CAKE_ORDERED:
+            return{
+                numOfCakes: state.numOfCakes-1
+            }
+            default:return state
+    }
+}
+const store =createStore(reducer); 
+console.log('Initial state',store.getState());
+
+const unsubscribe =store.subscribe(()=>console.log('update state',store.getState())) 
+store.dispatch({
+    type:CAKE_ORDERED,
+    quantity:1,
+}) 
+store.dispatch(orderCake())
+
+unsubscribe();
+console.log(store.getState());
+store.dispatch(orderCake())
